fcf01242710c34ceea32a4633b6d5577b44793ed
Brendan838 <Brendan838@gmail.com>
2021-11-30 21:16:45 -0600

    atlas setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require('path');
 const routes = require('./controllers');
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
+console.log(process.env.MONGODB_URI)
 app.use(routes);
 
 app.listen(PORT, () => {
